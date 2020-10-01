cd2b2e729fe7b331d866920f4e18ea80fb0d4665
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-10-01 23:03:22 +0530

    [Kirti]Added:Return Sad And Happy By Constructor

--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -6,15 +6,15 @@ import com.moodanalysernew.MoodAnalyser;
 public class MoodAnalyserTest {
 	@Test
 	public void givenMeassage_WhenSad_ShouldReturnSad() {
-		MoodAnalyser mooddetect = new MoodAnalyser();
-		String mood = mooddetect.analyseMood("This is sad message");
+		MoodAnalyser mooddetect = new MoodAnalyser("This is sad message");
+		String mood = mooddetect.analyseMood();
 		Assert.assertEquals("SAD", mood);
 	}
 
 	@Test
 	public void givenMessage_WhenNotSad_ShouldReturnHappy() {
-		MoodAnalyser mooddetect = new MoodAnalyser();
-		String mood = mooddetect.analyseMood("This is happy message");
+		MoodAnalyser mooddetect = new MoodAnalyser("This is happy message");
+		String mood = mooddetect.analyseMood();
 		Assert.assertEquals("HAPPY", mood);
 	}
 }
